796e87eea5a54d9f22e9b5bb91400565f8174753
Gangula Likhitha <likhithagangula5@gmail.com>
2026-06-18 11:33:34 +0530

    fixed Sidebar import naming

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import SideBar from "./components/Sidebar";
+import SideBar from "./components/SideBar";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
